1164468bde2d1e14e9db4a887814a1280790659d
Mll <1939356771@qq.com>
2026-07-05 19:47:28 +0800

    Restore navigation hover highlight

--- src/components/LibrarySidebar.vue
+++ src/components/LibrarySidebar.vue
@@ -369,7 +369,7 @@ function reset_playlist_drag() {
 
 .nav_item:hover,
 .nav_item.active {
-  color: var(--theme-title-color, #1e2026);
+  color: var(--theme-highlight-color, #426dff);
   background: #eaf0ff;
 }
 
